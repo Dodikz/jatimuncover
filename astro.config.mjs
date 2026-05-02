b8fc687bde25ae7db9e1110d80d385d663e5cd7b
Dodikz <sabdoilahidodik735@gmail.com>
2026-05-03 00:41:52 +0700

    feat: implement Navbar and Footer components with astro-icon integration and global CSS styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), icon()]
 });
